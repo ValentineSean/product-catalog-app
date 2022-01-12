0e394d27c73a4c786e9789c41a7ee41e11f04edb
Valentine Chanengeta <seanchanengeta@gmail.com>
2022-01-12 02:26:19 +0200

    design initiated

--- src/components/products/Products.vue
+++ src/components/products/Products.vue
@@ -5,7 +5,36 @@
         </div>
 
         <div v-if="!loading">
-            <h1 :style="{ margin: '12px 12px 0' }">Products</h1>
+            <a-row>
+                <a-col :span="12">
+                    <h1 :style="{ margin: '12px 12px 0' }">Products</h1>
+                </a-col>
+                <a-col :span="12">
+                    <span>
+                        <form>
+                            <form-item label="Search criteria">
+                                <a-select placeholder="Search criteria">
+                                    <a-select-option value="product">Product</a-select-option>
+                                    <a-select-option value="category">Category</a-select-option>
+                                    <a-select-option value="supplier">Supplier</a-select-option>
+                                </a-select>
+                            </form-item>
+
+                            <form-item>
+                                <a-auto-complete
+                                    v-model="searc_string"
+                                    :data-source="search_source"
+                                    style="width: 200px"
+                                    placeholder="input here"
+                                    @select="onSelect"
+                                    @search="onSearch"
+                                    @change="onChange"
+                                />
+                            </form-item>
+                        </form>
+                    </span>
+                </a-col>
+            </a-row>
             
             <a-layout-content :style="{ margin: '12px 12px 0' }">
                 <div :style="{ background: '#fff' }">
@@ -101,6 +130,16 @@
 <script>
     // import { mapActions } from "vuex"
 
+    const searchAll = (items, term) =>{
+        if(term){
+            return items.filter(item =>
+                ((item !== undefined && item !== null) && item.toLowerCase().includes(term.toLowerCase()))
+            )
+        }
+
+        return items
+    }
+
     export default{
         name: "Products",
 
@@ -109,7 +148,16 @@
                 loading: false,
                 createBtnDisabled: true,
                 rating_value: 3,
+                search_string: "",
+                data_source: ["mango", "banana"],
+                search_source: ["mango", "banana"],
             }
+        },
+
+        watch: {
+            search_string(val) {
+                console.log('value', val);
+            },
         },
 
         methods: {
@@ -125,7 +173,20 @@
 
             toFavorites(){
                 console.log("To favorites")
-            }
+            },
+
+            onSearch(search_text) {
+                // this.search_source = !search_text ? [] : [search_text, search_text.repeat(2), search_text.repeat(3)];
+                this.search_source = searchAll(this.data_source, search_text)
+            },
+
+            onSelect(value) {
+                console.log('onSelect', value);
+            },
+
+            onChange(value) {
+                console.log('onChange', value);
+            },
         },
 
         async created(){
